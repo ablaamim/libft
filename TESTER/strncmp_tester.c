45c449acf75b2133fa31e1438746a65f9dc5456f
ablaamim <aku-no-hana@outlook.fr>
2021-11-02 09:08:46 +0100

    modifications

---
+++ TESTER/strncmp_tester.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strncmp_tester.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 08:55:58 by ablaamim          #+#    #+#             */
+/*   Updated: 2021/11/02 09:08:25 by ablaamim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int	main(int argc, char **argv)
+{
+	(void)	argc;
+	(void)	argv;
+	int		ret_strncmp;
+	int		ret_ft_strncmp;
+	char	str0[] = {"Hello world!"};
+	char	str1[] = {"Hello world"};
+	char	str2[] = {"1337 BENGUERIR"};
+	char	str3[] = {"Can I help you?"};
+
+	/**************************************************************************/
+	printf("%s%s%s", BLUE, "-----------------------------------------------\n", DEFAULT);
+	printf("%s%s%s", BLUE, "\t TESTING YOUR STRCMP FUNCTION : \n", DEFAULT);
+	printf("%s%s%s", BLUE, "-----------------------------------------------\n", DEFAULT);
+	/**************************************************************************/
+// TEST 1
+	printf("%s%s%s", GREEN, "------------------ TEST 1 -------------------\n", DEFAULT);
+	ret_strncmp = strncmp(str0, str1, 5);
+	ret_ft_strncmp = ft_strncmp(str0, str1, 5);
+//	printf("ret_strncmp -> %d\nret_ft_strncmp -> %d\n", ret_strncmp, ret_ft_strncmp);
+	if (ret_strncmp == ret_ft_strncmp)
+		printf("%s%s%s", GREEN, "[OK]\n", DEFAULT);
+	else
+		printf("%s%s%s", RED, "[KO]\n", DEFAULT);
+// TEST 2
+	printf("%s%s%s", GREEN, "------------------ TEST 2 -------------------\n", DEFAULT);
+	ret_strncmp = strncmp(str0, str1, 13);
+	ret_ft_strncmp = ft_strncmp(str0, str1, 13);
+//	printf("ret_strncmp -> %d\nret_ft_strncmp -> %d\n", ret_strncmp, ret_ft_strncmp);
+	if (ret_strncmp == ret_ft_strncmp)
+		printf("%s%s%s", GREEN, "[OK]\n", DEFAULT);
+	else
+		printf("%s%s%s", RED, "[KO]\n", DEFAULT);
+// TEST 3
+    printf("%s%s%s", GREEN, "------------------ TEST 3 -------------------\n", DEFAULT);
+	ret_strncmp = strncmp(str2, str3, 0);
+	ret_ft_strncmp = ft_strncmp(str2, str3, 0);
+//	printf("ret_strncmp -> %d\nret_ft_strncmp -> %d\n", ret_strncmp, ret_ft_strncmp);
+	if (ret_strncmp == ret_ft_strncmp)
+		printf("%s%s%s", GREEN, "[OK]\n", DEFAULT);
+        else
+            printf("%s%s%s", RED, "[KO]\n", DEFAULT);
+// TEST 4
+    printf("%s%s%s", GREEN, "------------------ TEST 4 -------------------\n", DEFAULT);
+	ret_strncmp = strncmp(str2, str3, 100);
+	ret_ft_strncmp = ft_strncmp(str2, str3, 100);
+//	printf("ret_strncmp -> %d\nret_ft_strncmp -> %d\n", ret_strncmp, ret_ft_strncmp);
+	if (ret_strncmp == ret_ft_strncmp)
+		printf("%s%s%s", GREEN, "[OK]\n", DEFAULT);
+        else
+            printf("%s%s%s", RED, "[KO]\n", DEFAULT);
+// TEST 5
+    printf("%s%s%s", GREEN, "------------------ TEST 5 -------------------\n", DEFAULT);
+	ret_strncmp = strncmp(str0, str1, -5);
+	ret_ft_strncmp = ft_strncmp(str0, str1, -5);
+//	printf("ret_strncmp -> %d\nret_ft_strncmp -> %d\n", ret_strncmp, ret_ft_strncmp);
+	if (ret_strncmp == ret_ft_strncmp)
+		printf("%s%s%s", GREEN, "[OK]\n", DEFAULT);
+        else
+            printf("%s%s%s", RED, "[KO]\n", DEFAULT);
+	printf("%s%s%s", GREEN, "----------------------------------------------\n", DEFAULT);
+	printf("%s%s%s", BLUE, "------------------ FINISH --------------------\n", DEFAULT);
+	return (EXIT_SUCCESS);
+}
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -5,16 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/02 05:19:00 by ablaamim          #+#    #+#             */
-/*   Updated: 2021/11/02 05:51:57 by ablaamim         ###   ########.fr       */
+/*   Created: 2021/11/02 08:54:10 by ablaamim          #+#    #+#             */
+/*   Updated: 2021/11/02 08:55:11 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "libft.h"
 
 int	ft_strncmp(const char	*s1, const char	*s2, size_t	n)
 {
-	if (!n)
+	if (n == 0)
 		return (0);
 	while (*s1 && *s2 && *s1 == *s2 && --n)
 	{
