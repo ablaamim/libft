a356145f744f325aaae23c2b8c555149f1b697ce
ablaamim <aku-no-hana@outlook.fr>
2021-11-09 09:49:14 +0100

    moar

--- TESTER/calloc_tester.c
+++ TESTER/calloc_tester.c
@@ -6,7 +6,7 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 17:22:35 by ablaamim          #+#    #+#             */
-/*   Updated: 2021/11/09 08:49:28 by ablaamim         ###   ########.fr       */
+/*   Updated: 2021/11/09 09:38:46 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ int	main(int argc, char **argv)
 	ret_calloc = calloc(10, 4);
 	ret_ft_calloc = ft_calloc(10, 4);
 	if (!ft_memcmp(ret_calloc, ret_ft_calloc, (10 * 4)))
-		printf("\033[92mTest %2.i - OK \033[0m\n", 1);
+		printf("%s%s%s", GREEN, "[OK]\n", DEFAULT);
 	else
-		printf("\033[91mTest %2.i - KO \033[0m\n", 1);
+		printf("%s%s%s", RED, "[KO]\n", DEFAULT);
 	free(ret_calloc);
 	free(ret_ft_calloc);
 	printf("%s%s%s", GREEN, "---------------------------------------------\n", DEFAULT);
@@ -36,9 +36,9 @@ int	main(int argc, char **argv)
 	ret_calloc = calloc(10, 1);
 	ret_ft_calloc = ft_calloc(10, 1);
 	if (!ft_memcmp(ret_calloc, ret_ft_calloc, (10 * 1)))
-		printf("\033[92mTest %2.i - OK \033[0m\n", 2);
+		printf("%s%s%s", GREEN, "[OK]\n", DEFAULT);
 	else
-		printf("\033[91mTest %2.i - KO \033[0m\n", 2);
+		printf("%s%s%s", RED, "[KO]\n", DEFAULT);
 	free(ret_calloc);
 	free(ret_ft_calloc);
 	printf("%s%s%s", GREEN, "---------------------------------------------\n", DEFAULT);
